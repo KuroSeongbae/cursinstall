d2726150bd3209e860b5cf0884c58f2b84d7c431
seongbae <seongbae@vivaldi.net>
2024-02-01 23:06:28 +0100

    added to create package string

--- src/tui.rs
+++ src/tui.rs
@@ -1,6 +1,6 @@
-use cursive::Cursive;
+use cursive::{Cursive, View};
 use cursive::view::{Nameable, Resizable};
-use cursive::views::{Dialog, TextView, LinearLayout, EditView, ListView, Checkbox, PaddedView, Button};
+use cursive::views::{Dialog, TextView, LinearLayout, EditView, Checkbox, PaddedView, Button};
 
 use crate::helper;
 use crate::structs::Configuration;
@@ -33,7 +33,7 @@ pub fn create_menu_config(s: &mut Cursive, config: Configuration) {
     for package in config.packages.iter() {
         grouped_packages_view.add_child(LinearLayout::horizontal()
             .child(Checkbox::new().checked())
-            .child(TextView::new(package.clone())))
+            .child(TextView::new(package.clone())).with_name("package"))
     }
 
     for group in config.grouped_packages.iter()
@@ -45,7 +45,8 @@ pub fn create_menu_config(s: &mut Cursive, config: Configuration) {
             packages_view.add_child(
                     LinearLayout::horizontal()
                         .child(Checkbox::new().checked())
-                        .child(TextView::new(package.clone())));
+                        .child(TextView::new(package.clone())).with_name("package")
+                );
         }
 
         let packages_view = PaddedView::lrtb(5, 0, 0, 0, packages_view);
@@ -55,7 +56,7 @@ pub fn create_menu_config(s: &mut Cursive, config: Configuration) {
     }
 
     let buttons = LinearLayout::horizontal()
-        .child(Button::new("Install", |s| create_text_dialog(s, String::from("Installing"))))
+        .child(Button::new("Install", |s| create_install_dialog(s)))
         .child(Button::new("Back", |s| {
             s.pop_layer();
             create_path_dialog(s);
@@ -65,6 +66,43 @@ pub fn create_menu_config(s: &mut Cursive, config: Configuration) {
     grouped_packages_view.add_child(buttons);
 
     s.add_layer(grouped_packages_view)
+}
+
+pub fn create_install_dialog(s: &mut Cursive) {
+    let mut packages: String = String::new();
+
+    // TODO: check for correct type
+    s.call_on_all_named("package", |f: &mut LinearLayout| {
+        // let view: &mut LinearLayout = f.0;
+
+        match (f.get_child(0), f.get_child(1)) {
+            (Some(checkbox), Some(textview)) => {
+                let checkbox: &Checkbox = match checkbox.as_any().downcast_ref::<Checkbox>() {
+                    Some(cb) => cb,
+                    None => panic!("The Child is not a Checkbox"),
+                };
+
+                let textview: &TextView = match textview.as_any().downcast_ref::<TextView>() {
+                    Some(tv) => tv,
+                    None => panic!("The child is not a TextView"),
+                };
+
+                if checkbox.is_checked() {
+                    packages.push_str(format!("{} ", textview.get_content().source()).as_str());
+                }
+            },
+            _ => println!("meow"),
+        }
+    });
+
+    s.add_layer(Dialog::around(
+        TextView::new(packages)
+    )
+    .button("retry", |s| {
+            s.pop_layer();
+            create_path_dialog(s)
+    })
+    .button("exit", |s| s.quit())) 
 }
 
 pub fn create_text_dialog(s: &mut Cursive, text: String) {
